3982053bf08128fa9fc374cc778bbc3f87b990a7
liuliyuan520 <2541337855@qq.com>
2017-12-15 12:33:00 +0800

    面包屑完成

--- src/modules/breadcrumb/Breadcrumb.js
+++ src/modules/breadcrumb/Breadcrumb.js
@@ -7,59 +7,77 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 import { Breadcrumb} from 'antd';
 
-const removeParam = str =>{
-    let pathArr = str.split(':');
-    let prevPath = pathArr[0];
-    return pathArr.length > 1 ? prevPath.substring(0,prevPath.length-1) : prevPath.substring(0,prevPath.length);
+const getBreadcrumb=(breadcrumbNameMap, url)=>{
+    if (breadcrumbNameMap[url]) {
+        return breadcrumbNameMap[url];
+    }
+    const urlWithoutSplash = url.replace(/\/$/, '');
+    if (breadcrumbNameMap[urlWithoutSplash]) {
+        return breadcrumbNameMap[urlWithoutSplash];
+    }
+    let breadcrumb = '';
+    Object.keys(breadcrumbNameMap).forEach((item) => {
+        const itemRegExpStr = `^${item.replace(/:[\w-]+/g, '[\\w-]+')}$`;
+        const itemRegExp = new RegExp(itemRegExpStr);
+        if (itemRegExp.test(url)) {
+            breadcrumb = breadcrumbNameMap[item];
+        }
+    });
+    return breadcrumb;
+}
+
+const getNodeList =(nodeList)=>{
+    const arr = [];
+    nodeList.forEach((item) => {
+        if (item.to) {
+            return null;
+        }
+        if (item.children) {
+            arr.push({...item,children:null}, ...getNodeList(item.children));
+        }else{
+            arr.push(item);
+        }
+    });
+    return arr;
 }
 
 const VTaxBreadCrumb = props =>  {
-    const { location } = props;
-    const pathSnippets = location.pathname.split('/').filter(i => i);
-    const breadcrumbNameMap = {};
-    props.routes.forEach(item=>{
-        if(!item.to){
-            //要去掉url的:参数
-            breadcrumbNameMap[removeParam(item.path)] = item.name
-        }
-    })
-    breadcrumbNameMap['/web']='首页'
+    const { location,routes} = props;
+    const route = getNodeList(routes);
+    const breadcrumbNameMap = [];
 
-    if(pathSnippets[1]==='web'){
-        //如果是首页就踢出掉home，防止出现两个首页面包屑
-        pathSnippets.pop()
+    if (location && location.pathname) {
+        const pathSnippets = location.pathname.split('/').filter(i => i);
+
+        route.forEach(item=>{
+            breadcrumbNameMap[item.path] = item.name
+        })
+
+        const extraBreadcrumbItems = pathSnippets.map((_, index) => {
+            const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
+            const currentBreadcrumb = getBreadcrumb(breadcrumbNameMap, url);
+            const isLinkable = (index !== pathSnippets.length - 1);
+
+            return (
+                <Breadcrumb.Item key={url}>
+                    {
+                        //这条判断是为了在当前位置的时候禁止再点击当前位置的面包屑
+                        !isLinkable ? <span>{currentBreadcrumb}</span>  :  <Link to={url}>
+                            {currentBreadcrumb}
+                        </Link>
+                    }
+                </Breadcrumb.Item>
+            );
+        })
+        const breadcrumbItems = [].concat(extraBreadcrumbItems);
+        return (
+            pathSnippets.length > 1 && <Breadcrumb style={{margin:'10px 24px 0'}}>
+                {breadcrumbItems}
+            </Breadcrumb>
+        )
+
     }
 
-    //剔除掉路由表中没有的项
-    let newPathSnippets = pathSnippets.map((item,index)=>{
-        const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
-        return breadcrumbNameMap[url] ? item : false;
-    }).filter(i=>i)
-
-    console.log(newPathSnippets);
-
-
-    const extraBreadcrumbItems = newPathSnippets.map((_, index) => {
-        const url = `/${pathSnippets.slice(0, index + 1).join('/')}`;
-        //const name = props.routes.filter(item => item.path===url)[0].name
-        return <Breadcrumb.Item key={url}>
-            {
-                //这条判断是为了在当前位置的时候禁止再点击当前位置的面包屑
-                index ===  (newPathSnippets.length -1) ? <span>{breadcrumbNameMap[url]}</span>  : <Link to={url}>
-                    {breadcrumbNameMap[url]}
-                </Link>
-            }
-        </Breadcrumb.Item>
-    });
-
-    const breadcrumbItems = [].concat(extraBreadcrumbItems);
-
-    //下面的判断是为了让首页不显示面包屑
-    return ( newPathSnippets.length ===1 && newPathSnippets[0]==='dashboard' ) ? null : (
-        <Breadcrumb style={{margin:'10px 0'}}>
-            {breadcrumbItems}
-        </Breadcrumb>
-    )
 }
 
 export default VTaxBreadCrumb
