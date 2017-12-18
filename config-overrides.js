4a7097b160daec590e63b91e659ee9a110ff5a9f
liurunbin <365456507@qq.com>
2017-12-18 10:55:55 +0800

    overrides

--- config-overrides.js
+++ config-overrides.js
@@ -6,6 +6,20 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 //const rewireCssModules = require('react-app-rewire-css-modules');
+/**
+ * !!!!
+ * 出现Cannot read property 'exclude' of undefined时需要去
+ * react-app-rewire-less 中8 - 14行修改为
+ * const fileLoader = getLoader(
+ config.module.rules,
+ rule =>
+ rule.loader &&
+ typeof rule.loader === 'string' &&
+ (rule.loader.indexOf(`${path.sep}file-loader${path.sep}`) !== -1 ||
+ rule.loader.indexOf(`@file-loader${path.sep}`) !== -1)
+ );
+ * */
+
 
 module.exports = function override(config, env) {
     // do stuff with the webpack config...
