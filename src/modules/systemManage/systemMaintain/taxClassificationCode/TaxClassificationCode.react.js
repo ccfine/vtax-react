936a057d3b1193734a7f443ca42c6ee85d1751fb
liuliyuan520 <2541337855@qq.com>
2018-01-09 18:02:38 +0800

    1，进项税明细台账主界面 2，修改纳税主体和进项管理列表也面统一风格 3，给剩下的三级菜单加图标

---
+++ src/modules/systemManage/systemMaintain/taxClassificationCode/TaxClassificationCode.react.js
@@ -0,0 +1,16 @@
+/**
+ * author       : liuliyuan
+ * createTime   : 2017/12/16 17:47
+ * description  :
+ */
+import React, { Component } from 'react'
+import {Layout} from 'antd'
+
+class TaxClassificationCode extends Component {
+    render() {
+        return (
+            <Layout style={{background:'transparent'}} > 税收分类编码 </Layout>
+        )
+    }
+}
+export default TaxClassificationCode
--- src/modules/taxDeclare/children/routes.js
+++ src/modules/taxDeclare/children/routes.js
@@ -10,23 +10,36 @@ const CreateAMatter =()=><div>创建事项</div>
 const UpcomingMatter =()=><div>待办事项</div>
 const AlreadyMatter =()=><div>已办事项</div>
 
-const PATH = `/web/taxDeclare`;
+const ICON_URL_PATH = '/assets/routes_avatar/'
+const PATH = `/web/taxDeclare`
 
 const Children_Routes = [
     {
         path:`${PATH}/createAMatter`,
         component:wrapPage('创建事项',CreateAMatter),
         name:'创建事项',
+        icon:{
+            url:`${ICON_URL_PATH}createAMatter.svg`,
+            backgroundColor:'#56C7F3'
+        },
         exact:true,
     },{
         path:`${PATH}/upcomingMatter`,
         component:wrapPage('待办事项',UpcomingMatter),
         name:'待办事项',
+        icon:{
+            url:`${ICON_URL_PATH}upcomingMatter.svg`,
+            backgroundColor:'#F5A544'
+        },
         exact:true,
     },{
         path:`${PATH}/alreadyMatter`,
         component:wrapPage('已办事项',AlreadyMatter),
         name:'已办事项',
+        icon:{
+            url:`${ICON_URL_PATH}alreadyMatter.svg`,
+            backgroundColor:'#7FD62F'
+        },
         exact:true,
     },{
         path:`${PATH}`,
