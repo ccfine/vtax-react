fcd3e611505b75d3a2da45272054d7df6c07ea2c
liurunbin <365456507@qq.com>
2018-01-10 16:40:18 +0800

    1.税收分类编码bug修复 2.在做销项发票数据匹配列表

--- src/modules/vatManage/salesManag/salesInvoiceMatching/SalesInvoiceMatching.r.js
+++ src/modules/vatManage/salesManag/salesInvoiceMatching/SalesInvoiceMatching.r.js
@@ -4,6 +4,7 @@
 import React,{Component} from 'react'
 import { Tabs } from 'antd';
 import RoomTransactionFilePage from './roomTransactionFile'
+import InvoiceDataMatching from './invoiceDataMatching'
 const TabPane = Tabs.TabPane;
 
 function callback(key) {
@@ -12,11 +13,13 @@ function callback(key) {
 export default class SalesInvoiceMatching extends Component{
     render(){
         return(
-            <Tabs onChange={callback} type="card">
+            <Tabs onChange={callback} type="card" activeKey={'2'}>
                 <TabPane tab="房间交易档案" key="1">
                     <RoomTransactionFilePage />
                 </TabPane>
-                <TabPane tab="销项发票数据匹配列表" key="2">Content of Tab Pane 2</TabPane>
+                <TabPane tab="销项发票数据匹配列表" key="2">
+                    <InvoiceDataMatching />
+                </TabPane>
                 <TabPane tab="未匹配的发票列表" key="3">Content of Tab Pane 3</TabPane>
                 <TabPane tab="无需匹配的发票列表" key="4">Content of Tab Pane 3</TabPane>
             </Tabs>
