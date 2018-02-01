434dd94ddf523abd58fcdad0e8a1b04b953c910a
liurunbin <365456507@qq.com>
2018-02-01 10:30:32 +0800

    某个弹窗maskClosable重复

--- src/modules/vatManage/landPrice/landPriceManage/popModal/sourcePopModal/popModal.r.js
+++ src/modules/vatManage/landPrice/landPriceManage/popModal/sourcePopModal/popModal.r.js
@@ -59,7 +59,6 @@ export default class SourceModal extends React.Component{
     }
     render(){
         return (<Modal
-            maskClosable={false}
             destroyOnClose={true}
             title="土地价款来源"
             visible={this.props.visible}
