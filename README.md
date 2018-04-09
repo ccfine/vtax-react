9e747a95a28ab47d842633215762413dcdab832f
liuliyuan520 <2541337855@qq.com>
2018-04-09 18:11:45 +0800

    fix: 1，完善 README.md 里面的路由结构  2，把 polyfill.min.js 下载下来放在public/libs 里面 为了测试 react-loadable

--- README.md
+++ README.md
@@ -40,19 +40,19 @@
     - 其他台帐--``otherAccount``
         - 售房预缴台账--``prePaidHousingSales``
         - 预缴税款台账--``prepayTax``
-- 纳税申报
-    - 创建申报
-    - 查询申报
-- 报表管理
-    - 业务报表
-    - 管理分析报表
-- 系统管理
-    - 组织架构
-    - 用户权限
-    - 系统维护
-    - 接口管理
-    - 流程管理
-    - 系统监控
+- 纳税申报--``taxDeclare``
+    - 创建申报--``createADeclare``
+    - 查询申报--``searchDeclare``
+- 报表管理--``reportManage``
+    - 业务报表--``businessReport``
+    - 管理分析报表--``manageAnalysisReport``
+- 系统管理--``systemManage``
+    - 组织架构--``organization``
+    - 用户权限--``userPermissions``
+    - 系统维护--``systemMaintain``
+    - 接口管理--``interfaceManage``
+    - 流程管理--``processManage``
+    - 系统监控--``systemMonitor``
         
 
 ## Commitizen
--- src/modules/taxDeclare/children/routes.js
+++ src/modules/taxDeclare/children/routes.js
@@ -3,16 +3,26 @@
  * createTime   : 2017/12/15 18:04
  * description  :
  */
-import {wrapPage} from '../../../compoments'
-import CreateADeclare from '../../taxDeclare/createADeclare'
-import SearchDeclare from '../../taxDeclare/searchDeclare'
+import LoadAble from 'react-loadable'
+import {wrapPage,LoadingPage} from '../../../compoments'
+/*import CreateADeclare from '../../taxDeclare/createADeclare'*/
+/*import SearchDeclare from '../../taxDeclare/searchDeclare'*/
 const ICON_URL_PATH = '/assets/routes_avatar/'
 const PATH = `/web/taxDeclare`
 
+const AsyncCreateADeclare = LoadAble({
+    loader: () => import('../../taxDeclare/createADeclare'),
+    loading: LoadingPage,
+});
+
+const AsyncSearchDeclare = LoadAble({
+    loader: () => import('../../taxDeclare/searchDeclare'),
+    loading: LoadingPage,
+});
 const Children_Routes = [
     {
         path:`${PATH}/createADeclare`,
-        component:wrapPage('创建申报',CreateADeclare),
+        component:wrapPage('创建申报',AsyncCreateADeclare),
         name:'创建申报',
         icon:{
             url:`${ICON_URL_PATH}createAMatter.svg`,
@@ -21,7 +31,7 @@ const Children_Routes = [
         exact:true,
     },{
         path:`${PATH}/searchDeclare`,
-        component:wrapPage('查询申报',SearchDeclare),
+        component:wrapPage('查询申报',AsyncSearchDeclare),
         name:'查询申报',
         icon:{
             url:`${ICON_URL_PATH}upcomingMatter.svg`,
