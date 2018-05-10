2b2b487536203e1e488545ae595c334d01d74b2c
xiaoxiuchunzi <525319242@qq.com>
2018-05-10 20:21:19 +0800

    refactor(纳税申报): 申报办理页面调整

---
+++ src/modules/taxDeclare/createADeclare/applyDeclarationPopModal/data.js
@@ -0,0 +1,255 @@
+/*
+ * @Author: liuchunxiu 
+ * @Date: 2018-05-10 18:48:38 
+ * @Last Modified by: liuchunxiu
+ * @Last Modified time: 2018-05-10 20:15:39
+ */
+const one = [
+    [
+        {
+            num: "JXFPPP",
+            name: "财务凭证采集",
+            status: 0
+        },
+        {
+            num: "JXFPCJ",
+            name: "房间交易档案采集",
+            status: 0
+        },
+        {
+            num: "JXSEMXTZ",
+            name: "发票采集",
+            status: 0
+        },
+        {
+            num: "JXSEJGTZ",
+            name: "营业收入采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "销项发票采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "未开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "其它涉税调整台账",
+            status: 2
+        }
+    ]
+];
+
+
+const two = [
+    [
+        {
+            num: "JXFPPP",
+            name: "财务凭证采集",
+            status: 0
+        },
+        {
+            num: "JXFPCJ",
+            name: "房间交易档案采集",
+            status: 0
+        },
+        {
+            num: "JXSEMXTZ",
+            name: "发票采集",
+            status: 0
+        },
+        {
+            num: "JXSEJGTZ",
+            name: "营业收入采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "销项发票采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "未开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "其它涉税调整台账",
+            status: 2
+        }
+    ]
+];
+
+
+const three = [
+    [
+        {
+            num: "JXFPPP",
+            name: "财务凭证采集",
+            status: 0
+        },
+        {
+            num: "JXFPCJ",
+            name: "房间交易档案采集",
+            status: 0
+        },
+        {
+            num: "JXSEMXTZ",
+            name: "发票采集",
+            status: 0
+        },
+        {
+            num: "JXSEJGTZ",
+            name: "营业收入采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "销项发票采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "未开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "其它涉税调整台账",
+            status: 2
+        }
+    ]
+];
+
+const four = [
+    [
+        {
+            num: "JXFPPP",
+            name: "财务凭证采集",
+            status: 0
+        },
+        {
+            num: "JXFPCJ",
+            name: "房间交易档案采集",
+            status: 0
+        },
+        {
+            num: "JXSEMXTZ",
+            name: "发票采集",
+            status: 0
+        },
+        {
+            num: "JXSEJGTZ",
+            name: "营业收入采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "销项发票采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "未开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "其它涉税调整台账",
+            status: 2
+        }
+    ]
+];
+
+const five = [
+    [
+        {
+            num: "JXFPPP",
+            name: "财务凭证采集",
+            status: 0
+        },
+        {
+            num: "JXFPCJ",
+            name: "房间交易档案采集",
+            status: 0
+        },
+        {
+            num: "JXSEMXTZ",
+            name: "发票采集",
+            status: 0
+        },
+        {
+            num: "JXSEJGTZ",
+            name: "营业收入采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "销项发票采集",
+            status: 0
+        }
+    ],
+    [
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "未开票销售台账",
+            status: 1
+        },
+        {
+            num: "QTYWJXSEZCTZ",
+            name: "其它涉税调整台账",
+            status: 2
+        }
+    ]
+];
+
+const data = [one,two,three,four,five];
+export default data;
