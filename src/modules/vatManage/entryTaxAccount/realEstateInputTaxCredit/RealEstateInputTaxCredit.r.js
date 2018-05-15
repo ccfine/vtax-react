88a1113531c613ee3e342b5e946158918c34c303
liuliyuan520 <2541337855@qq.com>
2018-05-15 17:43:27 +0800

    fix(纳税申报): 修改 简易计税进项税额转出台账 和 不动产进项税额抵扣台账 的查询条件抽出来共用

--- src/modules/vatManage/entryTaxAccount/realEstateInputTaxCredit/RealEstateInputTaxCredit.r.js
+++ src/modules/vatManage/entryTaxAccount/realEstateInputTaxCredit/RealEstateInputTaxCredit.r.js
@@ -6,8 +6,60 @@ import { Tabs } from 'antd';
 import DeductibleInputTaxAmount from './deductibleInputTaxAmount'
 import FixedAssetsInputTaxDetails from './fixedAssetsInputTaxDetails'
 import SelfBuiltTransferFixedAssetsInputTaxDetails from './selfBuiltTransferFixedAssetsInputTaxDetails'
+import {getUrlParam} from 'utils'
+import moment from 'moment';
 const TabPane = Tabs.TabPane;
 
+const formItemStyle={
+    labelCol:{
+        span:8
+    },
+    wrapperCol:{
+        span:16
+    }
+}
+const searchFields=(disabled)=> {
+    return [
+        {
+            label:'纳税主体',
+            type:'taxMain',
+            fieldName:'mainId',
+            span:6,
+            componentProps:{
+                disabled,
+            },
+            formItemStyle,
+            fieldDecoratorOptions:{
+                initialValue: (disabled && getUrlParam('mainId')) || undefined,
+                rules:[
+                    {
+                        required:true,
+                        message:'请选择纳税主体'
+                    }
+                ]
+            },
+
+        }, {
+            label:'凭证月份',
+            type:'monthPicker',
+            formItemStyle,
+            span:6,
+            fieldName:'authMonth',
+            componentProps:{
+                disabled,
+            },
+            fieldDecoratorOptions:{
+                initialValue: (disabled && moment(getUrlParam('authMonth'), 'YYYY-MM')) || undefined,
+                rules:[
+                    {
+                        required:true,
+                        message:'请选择凭证月份'
+                    }
+                ]
+            }
+        }
+    ]
+}
 export default class RealEstateInputTaxCredit extends Component{
     state = {
         activeKey:'1'
@@ -19,16 +71,18 @@ export default class RealEstateInputTaxCredit extends Component{
     }
     render(){
         const {activeKey} = this.state;
+        const {search} = this.props.location;
+        let disabled = !!search;
         return(
             <Tabs onChange={this.onTabChange} type="card" activeKey={activeKey}>
                 <TabPane tab="待抵扣进项税额" key="1">
-                    <DeductibleInputTaxAmount />
+                    <DeductibleInputTaxAmount searchFields={searchFields(disabled)} />
                 </TabPane>
                 <TabPane tab="固定资产进项税额明细" key="2">
-                    <FixedAssetsInputTaxDetails />
+                    <FixedAssetsInputTaxDetails searchFields={searchFields(disabled)} />
                 </TabPane>
                 <TabPane tab="自建转自用固定资产进项税额明细" key="3">
-                    <SelfBuiltTransferFixedAssetsInputTaxDetails />
+                    <SelfBuiltTransferFixedAssetsInputTaxDetails searchFields={searchFields(disabled)} />
                 </TabPane>
             </Tabs>
         )
