b60d34fb4efecd8b74befdf7df5e8b3de8d284c9
xiaoxiuchunzi <525319242@qq.com>
2018-06-07 23:26:38 +0800

    fix(不动产进项税额抵扣台账 / 固定资产进项税额明细): 改回来

--- src/modules/vatManage/entryTaxAccount/realEstateInputTaxCredit/fixedAssetsInputTaxDetails/FixedAssetsInputTaxDetails.r.js
+++ src/modules/vatManage/entryTaxAccount/realEstateInputTaxCredit/fixedAssetsInputTaxDetails/FixedAssetsInputTaxDetails.r.js
@@ -101,17 +101,17 @@ const columns = context =>[
     {
         title: (
             <div className="apply-form-list-th">
-                <p className="apply-form-list-p1">自建转自用税额</p>
-                <p className="apply-form-list-p2">自建转自用税率</p>
+                <p className="apply-form-list-p1">税额</p>
+                <p className="apply-form-list-p2">税率</p>
             </div>
         ),
-        dataIndex: "selfTaxAmount",
+        dataIndex: "inTax",
         render: (text, record) => (
             <div>
                 <p className="apply-form-list-p1">{fMoney(text)}</p>
                 <p className="apply-form-list-p2">
                     {
-                        record.selfTax && `${record.selfTax}%`
+                        record.intaxRate && `${record.intaxRate}%`
                     }
                 </p>
             </div>
