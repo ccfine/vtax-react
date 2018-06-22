d2136f405cccd67c799181f1ee1f91532e254583
xiaoxiuchunzi <525319242@qq.com>
2018-06-22 20:44:47 +0800

    fix(其他业务进项税额转出台账): 日期改成月份，凭证号非必填

--- src/modules/vatManage/entryTaxAccount/otherBusinessInputTaxRollOut/OtherBusinessInputTaxRollOut.r.js
+++ src/modules/vatManage/entryTaxAccount/otherBusinessInputTaxRollOut/OtherBusinessInputTaxRollOut.r.js
@@ -2,7 +2,7 @@
  * @Author: liuchunxiu 
  * @Date: 2018-04-04 11:35:59 
  * @Last Modified by: liuchunxiu
- * @Last Modified time: 2018-05-28 15:40:28
+ * @Last Modified time: 2018-06-22 20:24:55
  */
 import React, { Component } from "react";
 import {connect} from 'react-redux';
@@ -158,7 +158,7 @@ const getColumns = (context,hasOperate) => {
         dataIndex: "voucherNum"
     },
     {
-        title: "日期",
+        title: "期间",
         dataIndex: "taxDate"
     },
     {
