ccdd7be4b2a56359c7baaee79016487cd904f62f
xiaoxiuchunzi <525319242@qq.com>
2018-06-25 11:16:41 +0800

    fix(纳税申报表): 百分比显示

--- src/modules/reportManage/businessReport/taxReturnForm/sheetData/sheet10.js
+++ src/modules/reportManage/businessReport/taxReturnForm/sheetData/sheet10.js
@@ -5,7 +5,7 @@ export default [
 		{ value: '预征项目和栏次', readOnly: true, rowSpan: 2, colSpan: 2 },
 		{ value: '销售额', readOnly: true },
 		{ value: '扣除金额', readOnly: true },
-		{ value: '预征率', readOnly: true },
+		{ value: '预征率（%）', readOnly: true },
 		{ value: '预征税额', readOnly: true }
 	],
 	[
