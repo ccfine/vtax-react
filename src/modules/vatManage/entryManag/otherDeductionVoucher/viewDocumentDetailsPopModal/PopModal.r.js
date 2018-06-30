4685db49dde433484a86c2566539c204da9ba293
xiaoxiuchunzi <525319242@qq.com>
2018-06-30 16:52:30 +0800

    fix(其它扣税凭证-查看凭证): 科目显示

--- src/modules/vatManage/entryManag/otherDeductionVoucher/viewDocumentDetailsPopModal/PopModal.r.js
+++ src/modules/vatManage/entryManag/otherDeductionVoucher/viewDocumentDetailsPopModal/PopModal.r.js
@@ -14,12 +14,12 @@ const columns = [{
 }, {
     title: '科目名称',
     dataIndex: 'debitSubjectName',
-    render:(text,record)=>text.trim()||record.creditSubjectName,
+    render:(text,record)=>(text.trim()!=='0' && text.trim()) || record.creditSubjectName,
     width:150
 }, {
     title: '科目代码',
     dataIndex: 'debitSubjectCode',
-    render:(text,record)=>text.trim()||record.creditSubjectCode,
+    render:(text,record)=>(text.trim()!=='0' && text.trim()) ||record.creditSubjectCode,
     width:150
 },{
     title: '借方金额',
