168228fda1e595384548a4a4b00794419c418bf6
xiaoxiuchunzi <525319242@qq.com>
2018-07-17 17:53:03 +0800

    fix(其他业务类型进出税额): 保存按钮权限

--- src/modules/vatManage/entryTaxAccount/otherBusinessInputTaxRollOut/OtherBusinessInputTaxRollOut.r.js
+++ src/modules/vatManage/entryTaxAccount/otherBusinessInputTaxRollOut/OtherBusinessInputTaxRollOut.r.js
@@ -2,7 +2,7 @@
  * @Author: liuchunxiu 
  * @Date: 2018-04-04 11:35:59 
  * @Last Modified by: liuchunxiu
- * @Last Modified time: 2018-07-11 17:34:55
+ * @Last Modified time: 2018-07-17 17:35:45
  */
 import React, { Component } from "react";
 import {connect} from 'react-redux';
@@ -187,7 +187,7 @@ class OtherBusinessInputTaxRollOut extends Component {
                                             type:'save',
                                             text:'保存',
                                             icon:'save',
-                                            userPermissions:['1401004'],
+                                            userPermissions:['1401003'],
                                             onClick:this.save,
                                             loading:saveLoding
                                         }],statusParam)
