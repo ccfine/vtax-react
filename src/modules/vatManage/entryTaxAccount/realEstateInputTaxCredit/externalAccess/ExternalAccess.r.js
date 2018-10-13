f730f99a517e8f7093b18285cf8b0dcf4cce7ee8
YOUGA-MAC <1115674625@qq.com>
2018-10-13 14:47:19 +0800

    feat: 新需求 不动产进项税额抵扣台账 页面

--- src/modules/vatManage/entryTaxAccount/realEstateInputTaxCredit/externalAccess/ExternalAccess.r.js
+++ src/modules/vatManage/entryTaxAccount/realEstateInputTaxCredit/externalAccess/ExternalAccess.r.js
@@ -3,129 +3,93 @@
  */
 import React, { Component } from 'react'
 import {SearchTable,TableTotal} from 'compoments'
-import {Form,message} from 'antd';
-import {fMoney,composeBotton,requestResultStatus,listMainResultStatus,request} from 'utils'
-import { NumericInputCell } from 'compoments/EditableCell'
-const columns = (context,isEdit) =>[
-    {
-        title:'纳税主体',
-        dataIndex: "taxSubjectName",
-        width:'150px',
+import {fMoney,composeBotton,requestResultStatus,listMainResultStatus} from 'utils'
+const columns = (context,isEdit) =>[{
+        title: '利润中心',
+        dataIndex: 'profitCenterName',
+        //width:'200px',
     },{
         title:'项目分期名称',
         dataIndex:'stageName',
-        width:'150px',
+        width:'200px',
     },{
         title:'固定资产名称',
         dataIndex:'assetName',
-        width:'150px',
+        width:'200px',
     },{
         title:'固定资产编号',
         dataIndex:'assetNo',
-        width:'100px',
+        width:'200px',
     },{
         title: "入账日期",
         dataIndex: "accountDate",
-        width:'100px',
+        width:'200px',
     },{
         title:'取得方式',
         dataIndex:'acquisitionMode',
-        render: (text, record) => {
-            // 0-外部获取
-            // 1-单独新建
-            // 2-自建转自用
-            let res = "";
-            switch (parseInt(text, 0)) {
-                case 0:
-                    res = "外部获取";
-                    break;
-                case 1:
-                    res = "单独新建";
-                    break;
-                case 2:
-                    res = "自建转自用";
-                    break;
-                default:
-                    break;
-            }
-            return res;
-        },
-        width:'100px',
+        width:'200px',
     },{
         title:'取得价值',
         dataIndex:'gainValue',
-        render:(text)=>fMoney(text),
-        className: "table-money",
+        width:'200px',
+    },
+    {
+        title: "资产类别",
+        dataIndex: "areaCovered",
+        width:'200px',
+    },
+    {
+        title: "资产状态",
+        dataIndex: "intaxRate",
+        width:'200px',
+    },
+    {
+        title: "占地面积",
+        dataIndex: "inTax",
         width:'100px',
     },
-    /*{
-     title: "建筑面积",
-     dataIndex: "areaCovered"
-     },*/
     {
-        title: "税率（%）",
-        dataIndex: "intaxRate",
-        render:(text,record,index)=>{
-            if(isEdit && record.intaxRateEdit){
-                return <NumericInputCell
-                    initialValue={text==='0' ? '0.00' : text}
-                    getFieldDecorator={context.props.form.getFieldDecorator}
-                    fieldName={`list[${index}].intaxRate`}
-                    editAble={true}
-                    componentProps={{decimalPlaces:10}}
-                />
-            }else{
-                return text && `${text}%`
-            }
-        },
+        title: "税率",
+        dataIndex: "taxAmount",
         width:'100px',
     },
     {
         title: "税额",
-        dataIndex: "inTax",
-        render:(text)=>fMoney(text),
-        className: "table-money",
-        width:'100px',
-    },
-    {
-        title: "当期抵扣的进项税额",
-        dataIndex: "taxAmount",
-        render:(text)=>fMoney(text),
-        className: "table-money",
-        width:'150px',
-    },
-    {
-        title: "待抵扣的进项税额",
         dataIndex: "deductedTaxAmount",
         render:(text)=>fMoney(text),
         className: "table-money",
         width:'150px',
     },{
-        title: "待抵扣期间",
+        title: "期初待抵扣进项税额",
         dataIndex: "deductedPeriod",
         width:'100px',
+        render:(text)=>fMoney(text),
+        className: "table-money",
     },{
-        title: "资产类别",
+        title: "当期抵扣进项税额",
         dataIndex: "assetType",
         width:'150px',
+        render:(text)=>fMoney(text),
+        className: "table-money",
     },{
-        title: "资产状态",
+        title: "期末待抵扣进项税额",
+        dataIndex: "assetsState",
+        width:'100px',
+        render:(text)=>fMoney(text),
+        className: "table-money",
+    },{
+        title: "待抵扣期间",
         dataIndex: "assetsState",
         width:'100px',
     },
 ];
-class FixedAssetsInputTaxDetails extends Component{
+export default class ExternalAccess extends Component{
     state={
         tableKey:Date.now(),
         filters: {},
         statusParam:{},
         dataSource:[],
-
-        saveLoading:false,
         totalSource:undefined,
-    }
-    toggoleSaveLoading=(saveLoading)=>{
-        this.mounted && this.setState({saveLoading})
     }
     fetchResultStatus = ()=>{
         requestResultStatus('/account/income/estate/listMain',this.state.filters,result=>{
@@ -134,37 +98,12 @@ class FixedAssetsInputTaxDetails extends Component{
             })
         })
     }
-    save=(e)=>{
-        e && e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                let params={...this.state.filters,list:[]};
-                values.list && values.list.forEach((ele,index)=>{
-                    params.list.push({id:this.state.dataSource[index].id,intaxRate:ele.intaxRate });
-                })
-                this.toggoleSaveLoading(true)
-                request.post('/account/income/estate/save',params)
-                    .then(({data})=>{
-                        this.toggoleSaveLoading(false)
-                        if(data.code===200){
-                            message.success(`保存成功！`);
-                            this.props.refreshTabs();
-                        }else{
-                            message.error(`保存失败:${data.msg}`)
-                        }
-                    }).catch(err=>{
-                    this.toggoleSaveLoading(false)
-                    message.error(`保存失败:${err.message}`)
-                })
-            }
-        })
-    }
     mounted = true;
     componentWillUnmount(){
         this.mounted = null;
     }
     render(){
-        const {tableKey,statusParam,filters,saveLoading,totalSource} = this.state;
+        const {tableKey,statusParam,filters,totalSource} = this.state;
         const { declare,searchFields } = this.props;
         let disabled = !!declare;
         return(
@@ -188,7 +127,6 @@ class FixedAssetsInputTaxDetails extends Component{
                         },()=>{
                             this.fetchResultStatus()
                         })
-                        this.props.form.resetFields();
                     }}
                     tableOption={{
                         key:tableKey,
@@ -196,7 +134,7 @@ class FixedAssetsInputTaxDetails extends Component{
                         columns:columns(this,disabled && declare.decAction==='edit' && parseInt(statusParam.status,10)===1),
                         url:'/account/income/estate/fixedList',
                         cardProps: {
-                            title: <span><label className="tab-breadcrumb">不动产进项税额抵扣台账 / </label>固定资产进项税额明细</span>,
+                            title: <span><label className="tab-breadcrumb">不动产进项税额抵扣台账 / </label>外部获取固定资产进项税额抵扣</span>,
                         },
                         // onSuccess: (params,dataSource) => {
                         //     this.setState({
@@ -205,22 +143,11 @@ class FixedAssetsInputTaxDetails extends Component{
                         //     },()=>{
                         //         this.fetchResultStatus()
                         //     })
-                        //     this.props.form.resetFields();
                         // },
                         extra: (
                             <div>
                                 {
                                     listMainResultStatus(statusParam)
-                                }
-                                {
-                                    disabled && declare.decAction==='edit' && parseInt(statusParam.status,10)===1 && composeBotton([{
-                                        type:'save',
-                                        text:'保存',
-                                        icon:'save',
-                                        userPermissions:['1251003'],
-                                        onClick:this.save,
-                                        loading:saveLoading
-                                    }],statusParam)
                                 }
                                 {
                                     JSON.stringify(filters) !=='{}' && composeBotton([{
@@ -284,7 +211,7 @@ class FixedAssetsInputTaxDetails extends Component{
                             })
                         },
                         scroll:{
-                            x:1700,
+                            x:2700,
                             y:window.screen.availHeight-430,
                         },
                     }}
@@ -293,5 +220,3 @@ class FixedAssetsInputTaxDetails extends Component{
         )
     }
 }
-
-export default Form.create()(FixedAssetsInputTaxDetails);
