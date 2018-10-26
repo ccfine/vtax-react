06adf540cb39ee74d13970955916910613efe829
liuliyuan520 <2541337855@qq.com>
2018-10-26 10:31:47 +0800

    fix: 利润中心纳税申报调试接口

--- src/modules/reportManage/businessReport/profitCenterTaxReturn/ProfitCenterTaxReturn.r.js
+++ src/modules/reportManage/businessReport/profitCenterTaxReturn/ProfitCenterTaxReturn.r.js
@@ -11,79 +11,61 @@ import sheet_2 from 'modules/reportManage/businessReport/taxReturnForm/sheetData
 import sheet_3 from 'modules/reportManage/businessReport/taxReturnForm/sheetData/sheet3'
 import sheet_4 from 'modules/reportManage/businessReport/taxReturnForm/sheetData/sheet4'
 import sheet_5 from 'modules/reportManage/businessReport/taxReturnForm/sheetData/sheet5'
-/*
-import sheet_6 from './sheetData/sheet6'
-import sheet_7 from './sheetData/sheet7'*/
 import {sheet_8,composeGrid_8} from 'modules/reportManage/businessReport/taxReturnForm/sheetData/sheet8'
-
-// import sheet_9 from './sheetData/sheet9'
 import sheet_10 from 'modules/reportManage/businessReport/taxReturnForm/sheetData/sheet10'
 import SheetWithSearchFields from './SheetWithSearchFields.r'
-import TaxCalculation from 'modules/reportManage/businessReport/taxReturnForm/taxCalculation'
+import TaxCalculation from './taxCalculation'
 const TabPane = Tabs.TabPane;
+
+const url = '/TaxDecConductPc/find',
+    saveUrl='/TaxDecConductPc/update';
 
 const sheetData = [
     {
         tab:'主表',
         grid:sheet_0,
-        url:'/tax/decConduct/main/list',
+        url,
+        saveUrl,
         action:true
     }, {
         tab:'附表一',
         grid:sheet_1,
-        url:'/tax/declaration/addendum/one/list',
+        url,
         scroll:{
             x:1800
         }
     }, {
         tab:'附表二',
         grid:sheet_2,
-        url:'/tax/declaration/addendum/two/list',
+        url,
     }, {
         tab:'附表三',
         grid:sheet_3,
-        url:'/tax/declaration/addendum/three/list',
-        saveUrl:'/tax/declaration/addendum/three/save',
+        url,
+        saveUrl,
     }, {
         tab:'附表四',
         grid:sheet_4,
-        url:'/tax/declaration/addendum/four/list',
-        //saveUrl:'/tax/declaration/addendum/four/save',
+        url,
     }, {
         tab:'附表五',
         grid:sheet_5,
-        url:'/tax/declaration/addendum/five/list',
-        saveUrl:'/tax/declaration/addendum/five/save',
-    /*}, {
-        tab:'固定资产表',
-        grid:sheet_6,
-        url:'/tax/decConduct/fixedAssets/list'
-    }, {
-        tab:'进项结构明细',
-        grid:sheet_7,
-        url:'/tax/declaration/tax/structure/list'*/
+        url,
+        saveUrl,
     },{
         tab:'增值税预缴税款表',
         grid:sheet_10,
-        url:'/tax/decConduct/prepayTax/list',
-        saveUrl:'/tax/decConduct/prepayTax/save',
+        url,
+        saveUrl,
     }, {
         tab:'增值税减免税申报明细表',
         grid:sheet_8,
-        url:'/tax/declaration/reduce/list',
+        url,
         composeGrid:composeGrid_8
     },{
         tab:'税款计算表',
         Component:TaxCalculation,
     },
-    // {
-    //     tab:'营改增税负分析测算明细表',
-    //     grid:sheet_9,
-    //     url:'/tax/decConduct/camping/list',
-    //     scroll:{
-    //         x:'200%'
-    //     }
-    // }
 ]
 class TaxReturnForm extends Component{
     state={
@@ -108,8 +90,8 @@ class TaxReturnForm extends Component{
                         <TabPane tab={item.tab} key={i}>
                             {
                                 parseInt(activeKey,0) === i ? (
-                                item.Component?<item.Component onParamsChange={this.onParamsChange} defaultParams={params}  declare={declare}/>:
-                                <SheetWithSearchFields {...item} onParamsChange={this.onParamsChange} defaultParams={params} declare={declare}/>
+                                item.Component?<item.Component onParamsChange={this.onParamsChange} defaultParams={params}  declare={declare} />:
+                                <SheetWithSearchFields {...item} onParamsChange={this.onParamsChange} defaultParams={params} declare={declare} reportType={i} />
                                 )
                                  : ''
                             }
