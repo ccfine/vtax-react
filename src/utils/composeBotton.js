ca267aa1ece1941762a87689da07a4ea22696288
ccfine <469781947@qq.com>
2018-10-29 17:28:33 +0800

    修复下载导入模板权限

--- src/utils/composeBotton.js
+++ src/utils/composeBotton.js
@@ -270,7 +270,7 @@ const composeBotton = (buttons = [], params) => {
                 break;
             case "fileDownload":
                 component = (
-                    <FileDownload { ...getFileDownloadOptions(item) } />
+                    <FileDownload { ...getFileDownloadOptions(item, params) } />
                 )
                 break
             case 'revokeImport':
