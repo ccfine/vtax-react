85113636478dfe6676301ee2b4a89e8a4a395966
liuliyuan520 <2541337855@qq.com>
2018-11-06 19:32:45 +0800

    fix: 修复

--- src/modules/basisManage/basicInfo/beginDataCollect/popModal/PopModal.r.js
+++ src/modules/basisManage/basicInfo/beginDataCollect/popModal/PopModal.r.js
@@ -2,44 +2,13 @@
  * Created by liuliyuan on 2018/5/20.
  */
 import React,{Component} from 'react';
-import {Form,Drawer,Row,Modal,message} from 'antd';
-import {AsyncTable} from 'compoments'
-import {getFields,request} from 'utils'
+import {Form,Drawer} from 'antd';
 import TabPage from './tabs.r'
-
-const getColumns = context =>[
-    {
-        title: '利润中心',
-        dataIndex: 'profitCenterName',
-    },{
-        title: '是否已采集',
-        dataIndex: 'finish',
-        width:'200px',
-        render:text=>{
-            //是否处理1:已采集 0:未采集 ,
-            let t = '';
-            switch (parseInt(text,0)){
-                case 0:
-                    t=<span style={{ color: '#44b973' }}>未采集</span>;
-                    break;
-                case 1:
-                    t=<span style={{ color: '#1795f6' }}>已采集</span>;
-                    break;
-                default:
-                //no default
-            }
-            return t
-        }
-    }
-];
 
 class PopModal extends Component{
     static defaultProps={
         type:'edit',
         visible:true,
-        beginType:'',
-        selectedRowKeys:[],
-        selectedRows:[],
     }
 
     state = {
@@ -56,53 +25,10 @@ class PopModal extends Component{
             visible
         })
     }
-    changeState = (value) =>{
-        this.setState({
-            beginType:value
-        },()=>{
-            value === '1' ?
-                this.setState({
-                    pageTabsKey:Date.now(),
-                })
-                :
-                this.setState({
-                    selectedRows:[],
-                },()=>{
-                    this.refreshTable()
-                });
-        })
-    }
-    deleteRecord = (type) =>{
-        const modalRef = Modal.confirm({
-            title: '友情提醒',
-            content: '更改采集维度后，以前的数据将会清除？',
-            okText: '确定',
-            okType: 'danger',
-            cancelText: '取消',
-            onOk:()=>{
-                modalRef && modalRef.destroy();
-                request.delete(`/dataCollection/delete/${this.props.modalConfig.record.mainId}/${type}`)
-                    .then(({data})=>{
-                        if(data.code===200){
-                            message.success('删除成功！');
-                            this.changeState(type)
-                        }else{
-                            message.error(`删除失败:${data.msg}`)
-                        }
-                    }).catch(err=>{
-                    message.error(err.message)
-                })
-            },
-            onCancel() {
-                modalRef.destroy()
-            },
-        });
-
-    }
     componentWillReceiveProps(nextProps){
         if(nextProps.visible && this.props.visible !== nextProps.visible){
             //传进来的是要求打开并且当前是关闭状态的时候，一打开就初始化
-            this.changeState(nextProps.modalConfig.record.type)
+            this.refreshTable()
         }
     }
     mounted=true;
@@ -110,13 +36,9 @@ class PopModal extends Component{
         this.mounted=null
     }
     render(){
-        const {tableKey,pageTabsKey,beginType,selectedRows} = this.state;
-        const {form,visible,modalConfig:{type,record}} = this.props;
-        const params = {
-            mainId:record && record.mainId,
-            profitCenterId: (beginType === '2' && this.state.selectedRows && this.state.selectedRows.length>0) ? this.state.selectedRows[0].profitCenterId : undefined,
-            //profitCenterName:this.state.selectedRows && this.state.selectedRows.length>0 ? this.state.selectedRows[0].profitCenterName : undefined
-        }
+        const {pageTabsKey} = this.state;
+        const {visible,modalConfig:{type,record}} = this.props;
+
         let title='';
         let disabled = false;
         switch (type){
@@ -151,78 +73,11 @@ class PopModal extends Component{
 
                 }}
             >
-                <Form>
-                    <Row>
-                        {
-                            getFields(form,[
-                                {
-                                    label: "期初数据采集维度",
-                                    fieldName: "type",
-                                    type: "select",
-                                    options: [
-                                        {
-                                            text: "纳税主体",
-                                            value: '1'
-                                        },
-                                        {
-                                            text: "利润中心",
-                                            value: '2'
-                                        }
-                                    ],
-                                    span: "8",
-                                    componentProps: {
-                                        disabled,
-                                        onChange:value=>{
-                                            this.deleteRecord(value)
-                                        }
-                                    },
-                                    fieldDecoratorOptions: {
-                                        initialValue: (record && record.type) || undefined,
-                                        rules: [
-                                             {
-                                                 required: true,
-                                                 message: '请选择期初数据采集维度'
-                                             }
-                                         ]
-                                     },
-                                }
-                            ])
-                        }
-                    </Row>
-                </Form>
                 <div style={{
                     height:window.screen.availHeight-250,
                     overflowY:'auto',
                 }}>
-                {
-                    (beginType && beginType === '2') && <AsyncTable url={`/dataCollection/pc/list/${record && record.mainId}`}
-                                                                        updateKey={tableKey}
-                                                                        tableProps={{
-                                                                            rowKey:record=>record.id,
-                                                                            pageSize:100,
-                                                                            columns:getColumns(this),
-                                                                            rowSelection:{
-                                                                                type: 'radio',
-                                                                            },
-                                                                            onRowSelect:(selectedRowKeys,selectedRows)=>{
-                                                                                this.setState({
-                                                                                    selectedRowKeys,
-                                                                                    selectedRows,
-                                                                                    pageTabsKey:Date.now(),
-                                                                                })
-                                                                            },
-                                                                            scroll:{
-                                                                                y:150,
-                                                                            },
-                                                                            style:{marginBottom:'20px'}
-                                                                    }} />
-                }
-                {
-                    ((selectedRows && selectedRows.length>0) && (beginType && beginType === '2')) && <TabPage key={pageTabsKey} filters={params} type={type} disabled={disabled} beginType={beginType} />
-                }
-                {
-                    (beginType && beginType === '1') && <TabPage key={pageTabsKey} filters={params} type={type} disabled={disabled} beginType={beginType} />
-                }
+                    <TabPage key={pageTabsKey} filters={record} type={type} disabled={disabled} beginType={this.props.tab} />
                 </div>
             </Drawer>
         )
---
+++ src/modules/basisManage/basicInfo/beginDataCollect/profitCenter/ProfitCenter.r.js
@@ -0,0 +1,180 @@
+/**
+ * Created by liuliyuan on 2018/11/6.
+ */
+import React, { Component } from 'react'
+import {message} from 'antd'
+import {SearchTable} from 'compoments'
+import PopModal from '../popModal'
+import {request,composeBotton} from 'utils';
+const pointerStyle = {
+    cursor:'pointer',
+    color:'#1890ff',
+    margin:'0px 5px'
+}
+
+const searchFields =(getFieldValue)=> [
+    {
+        label:'纳税主体',
+        fieldName:'main',
+        type:'taxMain',
+        span:6,
+        componentProps:{
+            labelInValue:true,
+        },
+        fieldDecoratorOptions:{
+            //initialValue: (disabled && {key:declare.mainId,label:declare.mainName}) || undefined,
+            rules:[
+                {
+                    required:true,
+                    message:'请选择纳税主体'
+                }
+            ]
+        }
+    }, {
+        label:'利润中心',
+        fieldName:'profitCenterId',
+        type:'asyncSelect',
+        span:6,
+        componentProps:{
+            fieldTextName:'profitName',
+            fieldValueName:'id',
+            //doNotFetchDidMount: !declare,
+            fetchAble: getFieldValue("main") && getFieldValue("main").key,
+            url:`/taxsubject/profitCenterList/${getFieldValue('main') && getFieldValue('main').key}`,
+        }
+    }
+]
+const getColumns = context =>[
+    {
+        title:'操作',
+        key:'actions',
+        render:(text,record)=>{
+            let submit = [
+                    {
+                        type:'action',
+                        title:'编辑',
+                        icon:'edit',
+                        userPermissions:['1121004'],
+                        onSuccess:()=>context.showModal('modify',record)
+                    },
+                    {
+                        type:'action',
+                        title:'提交',
+                        icon:'check',
+                        userPermissions:['1121010'],
+                        onSuccess:()=>context.handleSubmit('/dataCollection/submit','提交',record)
+                    }
+                ],
+                revoke = [
+                    {
+                        type:'action',
+                        title:'撤回提交',
+                        icon:'rollback',
+                        userPermissions:['1121011'],
+                        onSuccess:()=>context.handleSubmit('/dataCollection/revoke','撤回提交',record)
+                    }
+                ]
+            let showIcon = parseInt(record.status, 0) === 2 ? revoke : submit;
+            return composeBotton(showIcon)
+        },
+        fixed:'left',
+        width:'100px',
+        className:'text-center'
+    },{
+        title: '利润中心',
+        dataIndex: 'mainName',
+        render:(text,record)=>(<span title='查看详情' style={pointerStyle} onClick={()=>context.showModal('look',record)}>{text}</span>),
+    },{
+        title: '是否已采集',
+        dataIndex: 'finish',
+        render:text=>{
+            //是否处理1:已采集 0:未采集 ,
+            let t = '';
+            switch (parseInt(text,0)){
+                case 0:
+                    t=<span style={{ color: '#44b973' }}>未采集</span>;
+                    break;
+                case 1:
+                    t=<span style={{ color: '#1795f6' }}>已采集</span>;
+                    break;
+                default:
+                //no default
+            }
+            return t
+        }
+    }
+];
+
+export default class ProfitCenter extends Component{
+    state={
+        visible:false,
+        modalConfig:{
+            type:'',
+        },
+    }
+    refreshTable = ()=>{
+        this.setState({
+            tableKey:Date.now()
+        })
+    }
+    toggleModalVisible=visible=>{
+        this.setState({
+            visible
+        })
+    }
+    showModal=(type,record)=>{
+        this.toggleModalVisible(true)
+        this.setState({
+            modalConfig:{
+                type,
+                record
+            }
+        })
+    }
+
+    handleSubmit = (url,text,record) =>{
+        request.post(url,{
+            mainId:record.mainId,
+            mainName:record.mainId
+        })
+            .then(({data})=>{
+                if(data.code===200){
+                    message.success(`${text}成功!`);
+                    this.refreshTable();
+                }else{
+                    message.error(`${text}失败:${data.msg}`)
+                }
+            })
+            .catch(err=>{
+                message.error(err.message);
+            })
+    }
+
+    render(){
+        const {visible,modalConfig,tableKey} = this.state;
+        return(
+            <div className='oneLine'>
+            <SearchTable
+                style={{
+                    marginTop:-16
+                }}
+                doNotFetchDidMount={true}
+                searchOption={{
+                    fields:searchFields
+                }}
+                tableOption={{
+                    key:tableKey,
+                    pageSize:100,
+                    columns:getColumns(this),
+                    url:'/dataCollection/list',
+                    cardProps:{
+                        title:'利润中心期初数据采集',
+                    },
+                }}
+            >
+                <PopModal tab="2" visible={visible} refreshTable={this.refreshTable} modalConfig={modalConfig} toggleModalVisible={this.toggleModalVisible} />
+            </SearchTable>
+            </div>
+        )
+    }
+}
