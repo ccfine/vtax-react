9d8060fd5ef7ad88077576ae1cda5cf38ee196e9
webzhe <webzhe@163.com>
2018-11-22 16:55:40 +0800

    修复其他扣税凭证和进项税额明细台账 利润中心排版错误

--- src/modules/vatManage/entryTaxAccount/inputTaxDetails/InputTaxDetails.r.js
+++ src/modules/vatManage/entryTaxAccount/inputTaxDetails/InputTaxDetails.r.js
@@ -112,8 +112,13 @@ const checkVoucherType = record => {
 
 const getColumns = (context, isEdit) => {
     let lastStegesId = '';
-    const {dataSource, statusParam={}} = context.state;
+    const {dataSource=[], statusParam={}} = context.state;
     const {declare} = context.props;
+    let profitCenterList = [];
+    dataSource.forEach((item, index) => {
+        profitCenterList.includes(item.profitCenterId) ? profitCenterList.push(0) : profitCenterList.push(item.profitCenterId);
+        return item;
+    })
     return [
         {
             title: '利润中心',
@@ -121,12 +126,8 @@ const getColumns = (context, isEdit) => {
             width: '200px',
             render: (text, row, index) => {
                 let rowSpan = 0;
-                if (lastStegesId !== row.profitCenterId) {
-                    lastStegesId = row.profitCenterId;
-                    rowSpan = dataSource.filter(ele => ele.profitCenterId === row.profitCenterId).length;
-                }
-                if ((index + 1) === dataSource.length) {
-                    lastStegesId = '';
+                if(profitCenterList[index] === row.profitCenterId){
+                    rowSpan = dataSource.filter(ele=>ele.profitCenterId === row.profitCenterId).length;
                 }
                 return {
                     children: text,
